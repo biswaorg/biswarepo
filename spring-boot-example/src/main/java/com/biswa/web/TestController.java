32a3d58bd1c3698cbf0ddc582a6eb2454ff48c15
bmohapatra <bmohapatra@OSIT-L-0329.osius.com>
2016-09-15 16:58:14 +0530

    New Project For Cucumber with Spring Boot

---
+++ spring-boot-example/src/main/java/com/biswa/web/TestController.java
@@ -0,0 +1,13 @@
+package com.biswa.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "/")
+public class TestController {
+
+    @RequestMapping
+    public Response get() {
+        return new Response("Hello World!");
+    }
+}
---
+++ spring-boot-example/src/test/java/com/biswa/TestSuite.java
@@ -0,0 +1,30 @@
+package com.biswa;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.biswa.web.Response;
+
+import java.util.HashMap;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TestSuite {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void test() {
+        ResponseEntity<Response> response = this.restTemplate.getForEntity("/", Response.class, new HashMap<>());
+        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+        response.getBody().getMessage().equals("Hello World!");
+    }
+}
